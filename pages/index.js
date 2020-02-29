4cd6ce26aeb68310477e07e935b50c12b3b0951d
wnpraf <rapribu@gmail.com>
2020-02-29 14:26:57 +0100

    First commit

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default () => {
+  return (
+    <div className='flex justify-center items-center w-screen h-screen bg-red-200'>
+      <h1 className='text-5xl text-white'>Standar + tailwind = :)</h1>
+    </div>
+  )
+}
